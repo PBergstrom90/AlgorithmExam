7a23e0b61ec181db10f40aaa3a33f3e3b5051056
Pontus Bergström <pontusbergstrom90@gmail.com>
2023-12-27 12:55:21 +0100

    First Commit.

---
+++ main.cpp
@@ -0,0 +1,94 @@
+// PONTUS B. EXAM.
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+enum class SensorType
+{
+	Altitude,
+	SpeedInKmh,
+	FuelConsumption
+};
+
+class SensorData
+{
+	float value;
+	SensorType sensorType;
+	time_t time;
+public:
+	SensorType GetSensorType() { return sensorType; }
+	float GetValue() { return value; }
+	void SetValue(float v) { value = v; }
+	time_t GetTime() { return time; }
+	SensorData(SensorType sensorType, float value, time_t time)
+	{
+		this->value = value;
+		this->sensorType = sensorType;
+		this->time = time;
+	}
+};
+
+
+void FillData(vector<SensorData> &v);
+time_t CreateTime(int year, int month, int day, int hour, int minute, int second);
+
+int main()
+{
+	vector<SensorData> sensorData;
+	FillData(sensorData);
+
+	//SKRIV DIN KOD HÄR!!!!
+
+    // Del 1
+
+
+    // Del 2
+
+
+    // Del 3
+
+
+    return 0;
+}
+
+void FillData(vector<SensorData>& v)
+{
+	srand(time(NULL));
+
+	time_t tid = CreateTime(2012, 1, 1, 1, 1, 1 );
+	for (int i = 0; i < 100000; i++)
+	{
+		SensorType type = static_cast<SensorType>(rand() % 3);
+		float value = 0.0f;
+		if (type == SensorType::Altitude)
+			value = rand() % 1000;
+		else if (type == SensorType::FuelConsumption)
+			value = rand() * 3.0f;
+		else if (type == SensorType::SpeedInKmh)
+			value = rand() % 110;
+		else
+		{
+			value = 99;
+		}
+		v.push_back(SensorData(type,value,tid));
+		tid = tid + rand() % 10 + 1;
+	}
+}
+
+time_t CreateTime(int year, int month, int day, int hour, int minute, int second)
+{
+	struct tm tid = { 0 };
+	tid.tm_year = year-1900;
+	tid.tm_mon = month - 1;
+	tid.tm_mday = day;
+	tid.tm_hour = hour;
+	tid.tm_min = minute;
+	tid.tm_sec = second;
+	return mktime(&tid);
+}
