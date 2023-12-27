a3e86f65a1f0eaa2a45ff6f48615a44f662a56c1
Pontus Bergström <pontusbergstrom90@gmail.com>
2023-12-27 16:23:28 +0100

    Part 1 added.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <ctime>
 #include <cstdlib>
+#include <iomanip>
 
 using namespace std;
 
@@ -36,25 +37,79 @@ public:
 
 
 void FillData(vector<SensorData> &v);
+void SortAllData(vector<SensorData> &v);
+void PrintAllData(vector<SensorData> &v);
+void CheckSensorData(vector<SensorData> &v);
+void CheckTopSpeed(vector<SensorData> &v);
+void AdjustFuelConsumption(vector<SensorData> &v);
 time_t CreateTime(int year, int month, int day, int hour, int minute, int second);
 
 int main()
 {
 	vector<SensorData> sensorData;
+	// Datahandling
 	FillData(sensorData);
+	SortAllData(sensorData);
 
-	//SKRIV DIN KOD HÄR!!!!
+    // Part 1 
+	cout << "\nPart 1 - Check sensor data for altitude measurements: \n" << endl;
+ 	CheckSensorData(sensorData);	
 
-    // Del 1
+    // Part 2
+	cout << "\nPart 2 - Check top speed at 99.9: \n" << endl;	
+	CheckTopSpeed(sensorData);
 
+    // Part 3
+	cout << "\nPart 3 - Adjust fuel consumption: \n" << endl;
+	AdjustFuelConsumption(sensorData);
 
-    // Del 2
-
-
-    // Del 3
-
+	
 
     return 0;
+}
+
+void SortAllData(vector<SensorData> &v){
+	// Sort by timestamp:
+	sort(v.begin(), v.end(), [](SensorData& a, SensorData& b) { return a.GetTime() < b.GetTime(); });
+}
+
+void PrintAllData(vector<SensorData> &v){
+	// Print all data:
+
+}
+
+void CheckSensorData(vector<SensorData> &v) {
+    int count = 0;
+    time_t targetDate = CreateTime(2012, 1, 2, 0, 0, 0);
+
+    // Format the target date for better readability
+    struct tm* targetDateStruct = localtime(&targetDate);
+    stringstream formattedTargetDate;
+    formattedTargetDate << std::put_time(targetDateStruct, "%Y-%m-%d");
+
+    for(auto& data : v) {
+        if(data.GetSensorType() == SensorType::Altitude &&
+           data.GetTime() >= targetDate &&
+		   // Check if the timestamp falls within the target date
+           data.GetTime() < targetDate + 24 * 3600) {
+            // For printout:
+			// cout << "Altitude: " << data.GetValue() << " meters." << endl;
+            count++;
+        }
+    }
+	cout << "Date: " << formattedTargetDate.str() << endl;
+    cout << "Found " << count << " entries of altitude data." << endl;
+}
+
+
+
+
+void CheckTopSpeed(vector<SensorData> &v){
+	
+}	
+
+void AdjustFuelConsumption(vector<SensorData> &v){
+	
 }
 
 void FillData(vector<SensorData>& v)
