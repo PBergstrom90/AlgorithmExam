6dcb73e6bbb43b7e0bcc631005adb1e0e2b3c4a6
Pontus Bergström <pontusbergstrom90@gmail.com>
2023-12-28 14:00:40 +0100

    Implemented Part 2 and Part 3.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-// PONTUS B. EXAM.
-
+// Pontus B.
 #include <iostream>
 #include <string>
 #include <vector>
@@ -35,7 +34,6 @@ public:
 	}
 };
 
-
 void FillData(vector<SensorData> &v);
 void SortAllData(vector<SensorData> &v);
 void PrintAllData(vector<SensorData> &v);
@@ -44,26 +42,22 @@ void CheckTopSpeed(vector<SensorData> &v);
 void AdjustFuelConsumption(vector<SensorData> &v);
 time_t CreateTime(int year, int month, int day, int hour, int minute, int second);
 
+// DRIVER CODE
 int main()
 {
 	vector<SensorData> sensorData;
 	// Datahandling
 	FillData(sensorData);
 	SortAllData(sensorData);
-
-    // Part 1 
-	cout << "\nPart 1 - Check sensor data for altitude measurements: \n" << endl;
+ 
+	cout << "\nPART 1 - Check sensor data for altitude measurements: \n" << endl;
  	CheckSensorData(sensorData);	
 
-    // Part 2
-	cout << "\nPart 2 - Check top speed at 99.9: \n" << endl;	
+	cout << "\nPART 2 - Check top speed above 99.9 km/h: \n" << endl;	
 	CheckTopSpeed(sensorData);
 
-    // Part 3
-	cout << "\nPart 3 - Adjust fuel consumption: \n" << endl;
+	cout << "\nPART 3 - Increase fuel consumption by 75%: \n" << endl;
 	AdjustFuelConsumption(sensorData);
-
-	
 
     return 0;
 }
@@ -71,11 +65,6 @@ int main()
 void SortAllData(vector<SensorData> &v){
 	// Sort by timestamp:
 	sort(v.begin(), v.end(), [](SensorData& a, SensorData& b) { return a.GetTime() < b.GetTime(); });
-}
-
-void PrintAllData(vector<SensorData> &v){
-	// Print all data:
-
 }
 
 void CheckSensorData(vector<SensorData> &v) {
@@ -102,14 +91,44 @@ void CheckSensorData(vector<SensorData> &v) {
 }
 
 
-
-
 void CheckTopSpeed(vector<SensorData> &v){
-	
+	int count = 0;
+	int aboveSpeedLimit = 0;
+	for(auto& data : v) {
+		if(data.GetSensorType() == SensorType::SpeedInKmh) {
+			count++;
+			if(data.GetValue() > 99.9f) {
+				aboveSpeedLimit++;
+			}
+		}
+	}
+	cout << "Found " << count << " entries of 'Speed in Kilometers'." << endl;
+	cout << "Found " << aboveSpeedLimit << " entries above max speed (99.9 km/h)." << endl;
 }	
 
 void AdjustFuelConsumption(vector<SensorData> &v){
-	
+	int count = 0;
+	int maxEntries = 0;
+	bool success = false;
+
+	for(auto& data : v) {
+		if(data.GetSensorType() == SensorType::FuelConsumption) {
+			count++;	
+		}
+	}
+	cout << "Found " << count << " entries of 'Fuel Consumption'." << endl;	
+	cout << "\nAdjusting fuel consumption to increase by 75%." << endl;
+	for(auto& data : v) {
+		if(data.GetSensorType() == SensorType::FuelConsumption) {
+			data.SetValue(data.GetValue() * 1.75f);
+			success = true;	
+		}
+	}
+	if(success = true){
+		cout << "Adjusted " << count << " entries of 'Fuel Consumption' successfully." << endl;
+	} else {
+		cout << "Failed to adjust 'Fuel Consumption'." << endl;
+	}
 }
 
 void FillData(vector<SensorData>& v)
