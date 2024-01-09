12d13e58ef35be5c91cf6c7de173f1c1a0f65415
Pontus Bergström <pontusbergstrom90@gmail.com>
2024-01-09 15:05:25 +0100

    Reworked hockey.cpp & .h, to follow best practice and improve readability.

--- hockey.cpp
+++ hockey.cpp
@@ -6,6 +6,131 @@
 #include <random>
 #include "hockey.h"
 
+// HockeyPlayer-Class methods
+
+HockeyPlayer::HockeyPlayer(int id, std::string name, int jersey, std::string teamName) : id(id), name(name), jersey(jersey), teamName(teamName) {
+}
+HockeyPlayer::HockeyPlayer() : id(0), name("NULL"), jersey(0), teamName("NULL") {
+}
+int HockeyPlayer::getId() const {
+    return id;
+}
+std::string HockeyPlayer::getName() const {
+    return name;
+}
+int HockeyPlayer::getJersey() const {
+    return jersey;
+}
+std::string HockeyPlayer::getTeamName() const {
+    return teamName;
+}
+ // For reading from file.
+std::istream& operator>>(std::istream& is, HockeyPlayer& player) {
+    is >> player.id >> player.name >> player.jersey >> player.teamName;
+    return is;
+}
+// For writing to file.
+std::ostream& operator<<(std::ostream& os, const HockeyPlayer& player) {
+    os << player.id << ' ' << player.name << ' ' << player.jersey << ' ' << player.teamName;
+    return os;
+}
+
+// LRU-Class methods
+
+LRUCache::LRUCache(int capacity, std::vector<HockeyPlayer> cache) : capacity(capacity), cache(cache) {
+}
+
+bool LRUCache::isPlayerInCache(int playerId) {
+    return std::any_of(cache.begin(), cache.end(), [playerId](HockeyPlayer& player) {
+        return player.getId() == playerId;
+    });
+}
+
+HockeyPlayer LRUCache::getPlayerFromCache(int id) {
+    auto it = std::find_if(cache.begin(), cache.end(), [id](HockeyPlayer& player) {
+        return player.getId() == id;
+    });
+    if (it != cache.end()) {
+        HockeyPlayer player = *it;
+        cache.erase(it);
+        cache.push_back(player);
+        return player;
+    } else {
+        return HockeyPlayer();
+    }
+}
+
+void LRUCache::printCache() const {
+    for (const auto& player : cache) {
+        std::cout << "ID: " << player.getId() << '\n'
+                << "Name: " << player.getName() << '\n'
+                << "Jersey: " << player.getJersey() << '\n'
+                << "Team: " << player.getTeamName() << '\n'
+                << "--------------------------\n" << std::endl;
+    }
+}
+
+void LRUCache::addPlayer(HockeyPlayer& player) {
+    auto it = std::find_if(cache.begin(), cache.end(), [&player](HockeyPlayer& p) {
+        return p.getId() == player.getId();
+    });
+    // If Player is already in the cache, move it to the most recently used position.
+    if (it != cache.end()) {
+        cache.erase(it);
+        cache.push_back(player);
+    } else {
+        if (cache.size() >= capacity) {
+            // If the cache is full, remove the least recently used player
+            std::cout << "Removing least recently used player: " << cache.front().getId() << std::endl;
+            cache.erase(cache.begin());
+        }
+        // Player is not in the cache, add it.
+        cache.push_back(player);
+        std::cout << "Hockeyplayer added to cache: " << player.getId() << std::endl;
+    }
+}
+
+// FileStorage-class methods
+
+FileStorage::FileStorage(std::string fileName) : fileName(fileName) {
+}
+
+void FileStorage::addPlayer(const HockeyPlayer& player) {
+    playerStorage.push_back(player);
+}
+
+HockeyPlayer FileStorage::getPlayerFromTextFile(int id) {
+    std::ifstream file(fileName);
+    if (file.is_open()) {
+        HockeyPlayer player;
+        while (file >> player) {
+            if (player.getId() == id) {
+                // Close the file once the player is found.
+                file.close();
+                return player;
+            }
+        }
+        file.close();
+    } else {
+        std::cout << "Unable to open file: " << fileName << std::endl;
+    }
+    return HockeyPlayer();
+}
+
+void FileStorage::writeToFile() {
+    std::ofstream file(fileName);
+    if (file.is_open()) {
+        for (const auto& player : playerStorage) {
+            file << player << '\n';
+        }
+        file.close();
+        std::cout << "Players written to file: " << fileName << std::endl;
+    } else {
+        std::cout << "Unable to open file: " << fileName << std::endl;
+    }
+}
+
+// DRIVER CODE
 void HockeyChacher(){
     bool isRunning = true;
     std::cout << "\n--- Hockey LRU-Cache ---\n" << std::endl;
--- hockey.h
+++ hockey.h
@@ -1,103 +1,40 @@
 #ifndef __HOCKEY_H
-#define __HOCKEY_H  
+#define __HOCKEY_H
 
 #include <iostream>
-#include <fstream>
+#include <string>
 #include <vector>
+#include <fstream>
 #include <algorithm>
+#include <random>
 
 class HockeyPlayer {
 public:
-    HockeyPlayer(int id, std::string name, int jersey, std::string teamName) : id(id), name(name), jersey(jersey), teamName(teamName) {
-        }
-    HockeyPlayer() : id(0), name("NULL"), jersey(0), teamName("NULL") {
-        };
-    int getId() {
-        return id;
-    }   
-    std::string getName() {
-        return name;
-    }
-    int getJersey() {
-        return jersey;
-    }
-    std::string getTeamName() {
-        return teamName;
-    }
+    HockeyPlayer(int id, std::string name, int jersey, std::string teamName);
+    HockeyPlayer();
+    int getId() const;
+    std::string getName() const;
+    int getJersey() const;
+    std::string getTeamName() const;
 
-    // For reading from file.
-    friend std::istream& operator>>(std::istream& is, HockeyPlayer& player) {
-        is >> player.id >> player.name >> player.jersey >> player.teamName;
-        return is;
-    }
-
-    // For writing to file.
-    friend std::ostream& operator<<(std::ostream& os, const HockeyPlayer& player) {
-        os << player.id << ' ' << player.name << ' ' << player.jersey << ' ' << player.teamName;
-        return os;
-    }
+    friend std::istream& operator>>(std::istream& is, HockeyPlayer& player);
+    friend std::ostream& operator<<(std::ostream& os, const HockeyPlayer& player);
 
 private:
-        int id;
-        std::string name;
-        int jersey;
-        std::string teamName;
+    int id;
+    std::string name;
+    int jersey;
+    std::string teamName;
 };
 
 class LRUCache {
 public:
-    LRUCache(int capacity, std::vector<HockeyPlayer> cache) : capacity(capacity), cache(cache){
-    };
+    LRUCache(int capacity, std::vector<HockeyPlayer> cache);
+    bool isPlayerInCache(int playerId);
+    HockeyPlayer getPlayerFromCache(int id);
+    void printCache() const;
+    void addPlayer(HockeyPlayer& player);
 
-    bool isPlayerInCache(int playerId) {
-        return std::any_of(cache.begin(), cache.end(), [playerId](HockeyPlayer& player) {
-            return player.getId() == playerId;
-        });
-    }
-
-    HockeyPlayer getPlayerFromCache(int id) {
-        auto it = std::find_if(cache.begin(), cache.end(), [id](HockeyPlayer& player) {
-            return player.getId() == id;
-        });
-        if (it != cache.end()) {
-            HockeyPlayer player = *it;
-            cache.erase(it);
-            cache.push_back(player);
-            return player;
-        } else {
-            return HockeyPlayer();
-        }
-    }
-
-    void printCache() {
-    for (auto& player : cache) {
-    std::cout << "ID: " << player.getId() << '\n'
-            << "Name: " << player.getName() << '\n'
-            << "Jersey: " << player.getJersey() << '\n'
-            << "Team: " << player.getTeamName() << '\n'
-            << "--------------------------\n" << std::endl;
-        }
-    }
-
-    void addPlayer(HockeyPlayer& player) {
-    auto it = std::find_if(cache.begin(), cache.end(), [&player](HockeyPlayer& p) {
-        return p.getId() == player.getId();
-    });
-    if (it != cache.end()) {
-        // If Player is already in the cache, move it to the most recently used position
-        cache.erase(it);
-        cache.push_back(player);
-    } else {
-        // Player is not in the cache, add it
-        if (cache.size() >= capacity) {
-            // If the cache is full, remove the least recently used player
-            std::cout << "Removing least recently used player: " << cache.front().getId() << std::endl;
-            cache.erase(cache.begin());
-        }
-        cache.push_back(player);
-        std::cout << "Hockeyplayer added to cache: " << player.getId() << std::endl;
-    }
-}
 private:
     int capacity;
     std::vector<HockeyPlayer> cache;
@@ -105,45 +42,14 @@ private:
 
 class FileStorage {
 public:
-    FileStorage(std::string fileName) : fileName(fileName) {}
+    FileStorage(std::string fileName);
+    void addPlayer(const HockeyPlayer& player);
+    HockeyPlayer getPlayerFromTextFile(int id);
+    void writeToFile();
 
-    void addPlayer(const HockeyPlayer& player) {
-        playerStorage.push_back(player);
-    }
-
-    HockeyPlayer getPlayerFromTextFile(int id) {
-    std::ifstream file(fileName);
-    if (file.is_open()) {
-        HockeyPlayer player;
-        while (file >> player) {
-            if (player.getId() == id) {
-                file.close(); // Close the file once the player is found.
-                return player;
-            }
-        }
-        file.close();
-    } else {
-        std::cout << "Unable to open file: " << fileName << std::endl;
-    }
-        return HockeyPlayer();
-    }
-
-    void writeToFile() {
-    std::ofstream file(fileName);
-    if (file.is_open()) {
-        for (auto& player : playerStorage) {
-            file << player << '\n';
-        }
-        file.close();
-        std::cout << "Players written to file: " << fileName << std::endl;
-    } else {
-        std::cout << "Unable to open file: " << fileName << std::endl;
-        }
-    }
-
-private: 
+private:
     std::vector<HockeyPlayer> playerStorage;
     std::string fileName;
-
 };
+
 #endif // __HOCKEY_H
